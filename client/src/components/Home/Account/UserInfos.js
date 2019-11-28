71441a791a9fcf5145cfe39e7caaf7a2dfcaea80
= <=>
2019-11-28 19:04:06 -0300

    Pagina de usuário implementada

--- client/src/components/Home/Account/UserInfos.js
+++ client/src/components/Home/Account/UserInfos.js
@@ -6,17 +6,24 @@ import img_default from '../../assets/img_default.png'
 export default props => {
 
   const inf = props.inf
-  console.log(inf)
+
   return (
     <div className="Account-UserInfos">
         <div className="UserInfos-left">
-          <p>{inf.firstName} {inf.lastName}</p>
+          <p className="UserInfos-Fullname">{inf.firstName} {inf.lastName}</p>
+          <div className="UserInfos-details">
+            <p className="UserInfos-age">{/*inf.age ? inf.age : 'Age not informed'*/}18 Years</p>
+            <p className="UserInfos-location">{/*inf.location ? inf.location.contry + ' ' + inf.location.uf : 'Contry not informed'*/}Brazil, Rio Grande do Sul</p>
+            <p className="UserInfos-desc">"About me about me about me about me about me About me about me about me about me about me About me about me about me about me about me about about about"</p>
+          </div>
         </div>
         <div className="UserInfos-right">
           <div className="UserInfos-right-img">
             <img src={inf.image ? `${config.api}/public/user_images/${inf.image}` : img_default} />
+            <label htmlFor="Input-changeImg" className="UserInfos-changeImg">Change</label>
           </div>
-          <p>{inf.username} </p>
+          <input type="file" id="Input-changeImg" onChange={props.handleImageChange} />
+          <p>{inf.username}</p>
         </div>
         
     </div>
--- client/src/components/Home/Account/index.js
+++ client/src/components/Home/Account/index.js
@@ -14,12 +14,16 @@ export default props => {
     getProfile()
   }, [])
 
+  const handleImageChange = (e) => {
+    console.log('Changing the user image ...')
+  }
+
   const getProfile = () => {
     $.post(config.api + '/getUsers', {username: userInfos.username}, r => {
-      setProfile(r.r.map(data => (<UserInfos key={data._id} inf={data} />)))
+      setProfile(r.r.map(data => <UserInfos key={data._id} inf={data} handleImageChange={handleImageChange} />))
     });
   }
-  
+
   return (
     <div className="Account">
       {profile ? profile : null}
