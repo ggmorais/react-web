cc35e8e06194e11ac5674a368669d808dfe3d102
Gustavo Morais <gusxmorais@gmail.com>
2019-12-05 11:20:01 -0300

    pasta docs/ com o app buildado e pronto para servir no github pages

--- src/components/config.js
+++ src/components/config.js
@@ -1,10 +1,14 @@
 const home = {
-  api: 'http://10.0.0.15:3001'
+  api: 'http://10.0.0.15:5000'
 }
 
 const work = {
-  api: 'http://172.21.102.230:3001'
+  api: 'http://172.21.102.230:5000'
+}
+
+const prod = {
+  api: 'https://rw-api.herokuapp.com'
 }
 
 
-export default work;
+export default prod;
